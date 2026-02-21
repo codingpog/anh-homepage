5f7c72a5b21b9d57d76d59adadb611e342ed392c
Anh <vuanhkhoa22002@gmail.com>
2026-02-21 09:40:48 -0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# --- Stage 1: Builder ---
+FROM node:22
+
+# Set working directory for builder
+WORKDIR /app
+
+# Install deps
+COPY package*.json ./
+RUN npm install --omit=dev
+
+# Copy rest of the code
+COPY . .
+
+EXPOSE 5000
+
+CMD ["node", "server"]
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Server static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Main about page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Interests page
+app.get("/interests", (req, res) => {
+  res.sendFile(path.join(__dirname, "underconstruction.html"));
+});
+
+// WillAnhHitTheCorner page
+app.get("/willanhhitthecorner", (req, res) => {
+  res.sendFile(path.join(__dirname, "underconstruction.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
